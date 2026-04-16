95c7755b8eb6cc176c96447f2e72857ad59eac6c
trainee <deep.ai.gaurav@gmail.com>
2026-04-16 15:47:17 +0530

    indexes added

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,13 +1,69 @@
--- enable uuid generator
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-CREATE TABLE IF NOT EXISTS public.notebooks
+CREATE TABLE IF NOT EXISTS public.embeddings_test
 (
-    notebook_id uuid NOT NULL DEFAULT gen_random_uuid(),
-    notebook_name text COLLATE pg_catalog."default" NOT NULL,
+    embedding_id uuid NOT NULL DEFAULT gen_random_uuid(),
+    file_id uuid,
+    chunk_text text COLLATE pg_catalog."default",
+    embedding vector(1024),
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT notebooks_pkey PRIMARY KEY (notebook_id)
+    metadata jsonb,
+    text_search tsvector GENERATED ALWAYS AS (to_tsvector('english'::regconfig, chunk_text)) STORED,
+    CONSTRAINT embeddings_test_pkey PRIMARY KEY (embedding_id),
+    CONSTRAINT embeddings_test_file_id_fkey FOREIGN KEY (file_id)
+        REFERENCES public.files (file_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
 )
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.embeddings_test
+    OWNER to trainee;
+-- Index: embeddings_test_embedding_idx
+
+-- DROP INDEX IF EXISTS public.embeddings_test_embedding_idx;
+
+CREATE INDEX IF NOT EXISTS embeddings_test_embedding_idx
+    ON public.embeddings_test USING hnsw
+    (embedding vector_cosine_ops)
+    TABLESPACE pg_default;
+-- Index: text_search_idx
+
+-- DROP INDEX IF EXISTS public.text_search_idx;
+
+CREATE INDEX IF NOT EXISTS text_search_idx
+    ON public.embeddings_test USING gin
+    (text_search)
+    TABLESPACE pg_default;
+
+
+-- Table: public.files
+
+-- DROP TABLE IF EXISTS public.files;
+
+CREATE TABLE IF NOT EXISTS public.files
+(
+    file_id uuid NOT NULL DEFAULT gen_random_uuid(),
+    notebook_id uuid,
+    file_name text COLLATE pg_catalog."default" NOT NULL,
+    file_size integer,
+    file_status character varying(20) COLLATE pg_catalog."default",
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT files_pkey PRIMARY KEY (file_id),
+    CONSTRAINT files_notebook_id_fkey FOREIGN KEY (notebook_id)
+        REFERENCES public.notebooks (notebook_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.files
+    OWNER to trainee;
+
+
+-- Table: public.messages
+
+-- DROP TABLE IF EXISTS public.messages;
 
 CREATE TABLE IF NOT EXISTS public.messages
 (
@@ -25,42 +81,24 @@ CREATE TABLE IF NOT EXISTS public.messages
     CONSTRAINT messages_role_check CHECK (role::text = ANY (ARRAY['user'::character varying, 'assistant'::character varying, 'error'::character varying]::text[]))
 )
 
-CREATE TABLE IF NOT EXISTS public.files
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.messages
+    OWNER to trainee;
+
+-- Table: public.notebooks
+
+-- DROP TABLE IF EXISTS public.notebooks;
+
+CREATE TABLE IF NOT EXISTS public.notebooks
 (
-    file_id uuid NOT NULL DEFAULT gen_random_uuid(),
-    notebook_id uuid,
-    file_name text COLLATE pg_catalog."default" NOT NULL,
-    file_size integer,
-    file_status character varying(20) COLLATE pg_catalog."default",
+    notebook_id uuid NOT NULL DEFAULT gen_random_uuid(),
+    notebook_name text COLLATE pg_catalog."default" NOT NULL,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT files_pkey PRIMARY KEY (file_id),
-    CONSTRAINT files_notebook_id_fkey FOREIGN KEY (notebook_id)
-        REFERENCES public.notebooks (notebook_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
+    CONSTRAINT notebooks_pkey PRIMARY KEY (notebook_id)
 )
 
+TABLESPACE pg_default;
 
-CREATE TABLE IF NOT EXISTS public.embeddings_test
-(
-    embedding_id uuid NOT NULL DEFAULT gen_random_uuid(),
-    file_id uuid,
-    chunk_text text COLLATE pg_catalog."default",
-    embedding vector(1024),
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    metadata jsonb,
-    CONSTRAINT embeddings_test_pkey PRIMARY KEY (embedding_id),
-    CONSTRAINT embeddings_test_file_id_fkey FOREIGN KEY (file_id)
-        REFERENCES public.files (file_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
-)
-
--- Drop dependent tables first
--- DROP TABLE IF EXISTS files CASCADE;
--- DROP TABLE IF EXISTS messages CASCADE;
--- DROP TABLE IF EXISTS notebooks CASCADE;
--- DROP TABLE IF EXISTS embeddings CASCADE;
-
--- -- Optionally drop the extension if you no longer need UUID generation
--- DROP EXTENSION IF EXISTS "pgcrypto";
+ALTER TABLE IF EXISTS public.notebooks
+    OWNER to trainee;
